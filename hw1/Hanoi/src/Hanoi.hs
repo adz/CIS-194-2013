30a688eb8aa5c6a84bb8fb808568202591a460c4
Adam Davies <adzdavies@gmail.com>
2015-06-30 13:38:27 +0930

    Implement hanoi
    Also rearrange order of assertEqual since first is expectation

--- hw1/Hanoi/src/Hanoi.hs
+++ hw1/Hanoi/src/Hanoi.hs
@@ -7,4 +7,10 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi = undefined
+hanoi 0 _ _ _ = []
+hanoi numberOfDisks pegA pegB pegC =
+  moveAllButOneFromAtoCUsingB ++ moveTopFromAToB ++ moveAllButOneFromCtoBUsingA
+  where
+    moveAllButOneFromAtoCUsingB = hanoi (numberOfDisks - 1) pegA pegC pegB
+    moveTopFromAToB = [(pegA, pegB)]
+    moveAllButOneFromCtoBUsingA = hanoi (numberOfDisks - 1) pegC pegB pegA
--- hw1/Hanoi/src/HanoiTest.hs
+++ hw1/Hanoi/src/HanoiTest.hs
@@ -6,5 +6,10 @@ import Test.Framework
 
 test_HanoiWithTwoDisks :: IO ()
 test_HanoiWithTwoDisks = do
-  assertEqual (hanoi 21 "a" "b" "c") [("a","c"), ("a","b"), ("c","b")]
+  assertEqual expectedMoves (hanoi 2 "a" "b" "c") 
+  where expectedMoves = [("a","c"), ("a","b"), ("c","b")]
 
+test_HanoiWithThreeDisks :: IO ()
+test_HanoiWithThreeDisks = do
+  assertEqual expectedMoves (hanoi 3 "a" "b" "c")
+  where expectedMoves = [("a", "b"), ("a", "c"), ("b", "c"), ("a", "b"), ("c", "a"), ("c", "b"), ("a", "b")]
