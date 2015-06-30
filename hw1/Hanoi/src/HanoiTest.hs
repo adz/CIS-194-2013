67a608e399084bfee9057f96cc22fe58e512fae8
Adam Davies <adzdavies@gmail.com>
2015-06-30 13:41:47 +0930

    Added test asserted by exercies (15 disks = 32767 moves)

--- hw1/Hanoi/src/HanoiTest.hs
+++ hw1/Hanoi/src/HanoiTest.hs
@@ -13,3 +13,8 @@ test_HanoiWithThreeDisks :: IO ()
 test_HanoiWithThreeDisks = do
   assertEqual expectedMoves (hanoi 3 "a" "b" "c")
   where expectedMoves = [("a", "b"), ("a", "c"), ("b", "c"), ("a", "b"), ("c", "a"), ("c", "b"), ("a", "b")]
+
+test_MovesFor15Disks :: IO ()
+test_MovesFor15Disks = do
+  assertEqual 32767 movesFor15Disks
+  where movesFor15Disks = length $ hanoi 15 "a" "b" "c"
