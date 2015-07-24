9ee7e09bab11f2273cccd0aaf8b39e98b8d04827
Adam Davies <adzdavies@gmail.com>
2015-07-24 15:27:30 +0930

    Cleaned up old code & comments

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -1,31 +1,6 @@
 import Data.List
 
--- First go:
-{-skips :: [a] -> [[a]]-}
-{-skips xs = take (length xs) $ gen 0 xs-}
-
-{-gen :: Integer -> [a] -> [[a]]-}
-{-gen n xs = s n xs : gen (n + 1) xs-}
-
-{-s :: Int -> [a] -> [a]-}
-{-s n xs = case drop n xs of-}
-           {-[] -> []-}
-           {-(x:xs') -> x : s n xs'-}
-
--- Second go:
-{-skips :: [a] -> [[a]]-}
-{-skips xs = take (length xs) $ map (`s` xs) [0..]-}
-
-{-s :: Int -> [a] -> [a]-}
-{-s n xs = case drop n xs of-}
-           {-[] -> []-}
-           {-(x:xs') -> x : s n xs'-}
-
-
-
-{-gen n xs = unfoldr (\(n, xs) -> Just ((s n, xs), (s (n+1) xs)))-}
-
--- 3
+-- Ex1 skips
 skips :: [a] -> [[a]]
 skips xs = map (`s` xs) [0..length xs - 1]
 
@@ -34,14 +9,12 @@ s n xs = case drop n xs of
            [] -> []
            (x:xs') -> x : s n xs'
 
-
 -- unused... so far
 -- thinking to get list of indexes, then map over that...
 indexes :: Int -> Int -> Int -> [Int]
 indexes cur inc tot
   | cur + inc < tot = cur + inc : indexes (cur + inc + 1) inc tot
   | otherwise = []
-
 
 
 
@@ -56,8 +29,6 @@ m a b c = [b | b > a && b > c]
 
 
 -- Ex3 Histogram
-{-histogram :: [Integer] -> String-}
--- so far... this one converts to counts
 histogram :: [Integer] -> String
 histogram xs = intercalate "\n" $ graph ++ [key]
   where graph = map (`mapLine` counts) (maxDown counts)
